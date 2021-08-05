dd8bd6e65fa327cadfd0165e44e000dfd67a9d62
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2021-08-05 14:58:22 +0430

    router post added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express") ;
 const app = express() ;
 const db = require("./models")
+
+//Router 
+const postRouter = require("./routes/Posts.js")
+app.use("/posts", postRouter)
+
 db.sequelize.sync().then(()=>{
     
 app.listen(3001 , ()=>{
@@ -8,9 +13,5 @@ app.listen(3001 , ()=>{
    
 })
 
-app.get("/" , (req,res)=>{
-    res.send("hello world")
-}).catch((e)=>{
-    console.log(e)
-})
+
 })
